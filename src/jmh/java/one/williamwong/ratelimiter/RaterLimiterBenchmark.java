2c1c574e7c77c08e29dead6b9310fe80255a8e9c
William Wong <william1104@gmail.com>
2020-12-22 00:59:36 +0000

    update jmh testcase

--- src/jmh/java/one/williamwong/ratelimiter/RaterLimiterBenchmark.java
+++ src/jmh/java/one/williamwong/ratelimiter/RaterLimiterBenchmark.java
@@ -1,69 +1,61 @@
 package one.williamwong.ratelimiter;
 
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import static java.time.Duration.ofMillis;
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
 
-@State(Scope.Thread)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Fork(3)
-@BenchmarkMode(Mode.AverageTime)
+@BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(MICROSECONDS)
 public class RaterLimiterBenchmark {
-    @Param({"1", "10", "100"})
-    private Integer numOfInvokers;
 
-    @Param({"100", "10000", "1000000"})
-    private Integer maxInvokes;
-
-    @Param({"10", "1000"})
-    private Long durationInMs;
-
-    @Param({"SimpleRateLimiter", "StampLockRateLimiter"})
-    private String rateLimiterType;
-
-    private IRateLimiter rateLimiter;
-
-    @Setup
-    public void setup() {
-        switch (rateLimiterType) {
-            case "SimpleRateLimiter":
-                rateLimiter = new SimpleRateLimiter(maxInvokes, Duration.ofMillis(durationInMs));
-                break;
-            case "StampLockRateLimiter":
-                rateLimiter = new StampLockRateLimiter(maxInvokes, Duration.ofMillis(durationInMs));
-                break;
-
-            default:
-                throw new IllegalStateException("Failed to initialize rate limiter");
-        }
+    @Group("singleThread")
+    @GroupThreads(1)
+    @Benchmark
+    public void singleThread(RateLimiterWrapper rateLimiterWrapper) {
+        rateLimiterWrapper.rateLimiter.acquire();
     }
 
+    @Group("tenThread")
+    @GroupThreads(10)
     @Benchmark
-    public void test(Blackhole bh) throws ExecutionException, InterruptedException {
+    public void tenThread(RateLimiterWrapper rateLimiterWrapper) {
+        rateLimiterWrapper.rateLimiter.acquire();
+    }
 
-        rateLimiter.reset();
-        List<CompletableFuture> futures = new ArrayList<>();
+    @Group("hundredThread")
+    @GroupThreads(100)
+    @Benchmark
+    public void hundredThread(RateLimiterWrapper rateLimiterWrapper) {
+        rateLimiterWrapper.rateLimiter.acquire();
+    }
 
-        for (int i = 0; i < numOfInvokers; i++) {
-            futures.add(CompletableFuture.runAsync(() -> {
-                int numOfInvocations = maxInvokes / numOfInvokers;
-                for (int j = 0; j < numOfInvocations; j++) {
-                    rateLimiter.acquire();
-                }
-            }));
+
+    @State(Scope.Group)
+    public static class RateLimiterWrapper {
+        @Param({"SimpleRateLimiter", "StampLockRateLimiter"})
+        public String rateLimiterType;
+
+        public IRateLimiter rateLimiter;
+
+        @Setup
+        public void setup() {
+            switch (rateLimiterType) {
+                case "SimpleRateLimiter":
+                    rateLimiter = new SimpleRateLimiter(1_000_000, ofMillis(1));
+                    break;
+                case "StampLockRateLimiter":
+                    rateLimiter = new StampLockRateLimiter(1_000_000, ofMillis(1));
+                    break;
+                default:
+                    throw new IllegalStateException("Failed to initialize rate limiter");
+            }
         }
-
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[1])).get();
     }
 
 }
